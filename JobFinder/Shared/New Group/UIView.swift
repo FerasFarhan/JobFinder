da2597c85a447c9be3f8c1fce4fcbb2e65895265
Feras Farhan <ferasfarhan@live.com>
2019-02-18 13:42:08 +0200

    Working on filter view controller

---
+++ JobFinder/Shared/New Group/UIView.swift
@@ -0,0 +1,113 @@
+//
+//  UIView.swift
+//  Meeza
+//
+//  Created by Feras Farhan on 07/10/19.
+//  Copyright © 2019 Techband. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable
+    var cornersRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor(cgColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowColor = UIColor.black.cgColor
+            layer.shadowOffset = CGSize(width: 0, height: 2)
+            layer.shadowOpacity = 0.4
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable
+    var isAddShadow: Bool {
+        get {
+            return false
+        }
+        set {
+            if isAddShadow {
+                layer.masksToBounds = false
+                layer.shadowColor = UIColor.black.cgColor
+                layer.shadowOpacity = 1
+                layer.shadowOffset = CGSize(width: 2, height: 2)
+                layer.shadowRadius = 5
+
+                layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+                layer.shouldRasterize = true
+                layer.rasterizationScale = 1
+            }
+        }
+    }
+
+    
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
+    
+    func roundCorners(_ corners:UIRectCorner,_ cormerMask:CACornerMask, radius: CGFloat) {
+        if #available(iOS 11.0, *){
+            self.clipsToBounds = false
+            self.layer.cornerRadius = radius
+            self.layer.maskedCorners = cormerMask
+        }else{
+            let rectShape = CAShapeLayer()
+            rectShape.bounds = self.frame
+            rectShape.position = self.center
+            rectShape.path = UIBezierPath(roundedRect: self.bounds,    byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius)).cgPath
+            self.layer.mask = rectShape
+        }
+    }
+
+    func shake(count : Float = 4,for duration : TimeInterval = 0.5,withTranslation translation : Float = 5) {
+
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        animation.repeatCount = count
+        animation.duration = duration/TimeInterval(animation.repeatCount)
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: CGFloat(-translation), y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: CGFloat(translation), y: self.center.y))
+        layer.add(animation, forKey: "shake")
+    }
+    
+}
